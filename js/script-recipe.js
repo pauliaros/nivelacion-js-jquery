45cd8c00a52ea36e52880a95d188cd1089d64334
Paulina Aros <aros.pau@gmail.com>
2017-07-10 11:27:24 -0400

    se oculta flecha de index y barra de menu de recipes

---
+++ js/script-recipe.js
@@ -0,0 +1,7 @@
+$(document).ready( function(){
+	$(".js-menu").hide();
+
+	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
+	renderHighlightedRecipes(recipesArray);
+
+});
