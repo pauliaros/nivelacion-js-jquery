2e7b332e5903c15f4866c6bb433c8dfcb798b524
Paulina Aros <aros.pau@gmail.com>
2017-07-10 13:54:47 -0400

    intento fracasado de etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -35,6 +35,21 @@ function renderHighlightedRecipes(recipesArray) {
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
+	$(".item-recipe").append("<a class='item-recipe' href='#'>  <span class='attribution'>"
+    "<span class='title-recipe'>" +title.recipe +"</span>"
+    "<span class='metadata-recipe'>"
+      "<span class='author-recipe'>" +  recipe.source.name+"</span>"
+      "<span class='bookmarks-recipe'>"
+        "<span class='icon-bookmark'>" "</span>" 
+      </span>
+    </span>
+  </span>
+
+  "<img src='img/recipes/640x480'" + name.recipe + ".jpg" + "/>"
+</a>
+"
+
+		)
 	console.log('Voy a pintar la receta: ', recipe);
 }
 
